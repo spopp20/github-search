707df14e7b0f32996f147c26a33d5375f49f3e6e
spopp20 <stephen.popp20@gmail.com>
2020-03-26 20:46:33 -0400

    Using the GitHup API to get data - with axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import NavBar from './components/layouts/NavBar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+    //console.log(res.data);
+  }
 
   render() {
     return (
       <div className = 'App'>
         <NavBar title='GitHub Search' icon='fab fa-github' />
         <div className='container'>
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -9,7 +9,7 @@ const UserItem = ({user: {login, avatar_url, html_url }}) => {
             <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }}/>
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">Profile</a>
+              <a href={html_url} className="btn btn-dark btn-sm my-1">Profile</a>
             </div>
         </div>  
     );
