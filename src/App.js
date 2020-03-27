5c681c6367713a653f178f209eaec3b277c3a858
spopp20 <stephen.popp20@gmail.com>
2020-03-27 13:15:13 -0400

    Added private client_id and client_secret and added a clear button - downside - passing around props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from './components/layouts/NavBar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -9,20 +10,35 @@ class App extends Component {
   state = {
     users: [],
     loading: false
-  }
+  };
+
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
     this.setState({ users: res.data, loading: false });
-    //console.log(res.data);
   }
 
+  // Search GitHub users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
+    this.setState({ users: res.data.items, loading: false });
+  }
+
+  clearUsers = () => this.setState({  users: [], loading: false });
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className = 'App'>
         <NavBar title='GitHub Search' icon='fab fa-github' />
         <div className='container'>
-          <Users users={this.state.users} loading={this.state.loading} />
+          <Search 
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true: false}
+          />
+          <Users users={users} loading={loading} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from "./UserItem";
+import Spinner from '../layouts/Spinner';
+import PropTypes from 'prop-types'
 
-export class Users extends Component {
-    render() {
-        return (
-            <div style={userStyle}>
-              {this.props.users.map(user => (
-                  <UserItem key={user.id} user={user}/>
-              ))}  
-            </div>
-        )
-    }
+
+// Convert to functional component
+const Users = ({ users, loading }) => {
+
+    if (loading) {
+        return <Spinner />
+    } else {
+      return (
+        <div style={userStyle}>
+            {users.map(user => (
+                <UserItem key={user.id} user={user}/>
+            ))}  
+        </div>
+        );
+    }  
+}
+
+UserItem.defaultProps = {
+    users: [],
+    loading: false
+};
+
+UserItem.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
