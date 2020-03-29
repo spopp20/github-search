2264a0262ee3edaa9b8efb78f78255ac1e809354
spopp20 <stephen.popp20@gmail.com>
2020-03-29 16:13:21 -0400

    Change not found message

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>Not Found</h1>
-      <p className='lead'>The page you are looking for does not exist...</p>
+      <p className='lead'>The page you are looking for was not found.</p>
     </div>
   );
 };
